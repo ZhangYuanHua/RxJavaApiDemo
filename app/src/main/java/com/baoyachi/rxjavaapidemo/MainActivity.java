a5003642bc9ba18d6223e676e5e5b27fc6172251
baoyachi <liaoymxsdl@sina.com>
2016-07-17 16:52:47 +0800

    add CreateActivity

--- app/src/main/java/com/baoyachi/rxjavaapidemo/MainActivity.java
+++ app/src/main/java/com/baoyachi/rxjavaapidemo/MainActivity.java
@@ -1,15 +1,32 @@
 package com.baoyachi.rxjavaapidemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.baoyachi.rxjavaapidemo.create.CreateActivity;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private TextView mTvCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    private void init()
+    {
+        mTvCreate = (TextView) findViewById(R.id.tv_create);
+        mTvCreate.setOnClickListener(view -> {
+            startActivity(new Intent(getBaseContext(), CreateActivity.class));
+        });
+    }
+
 }
