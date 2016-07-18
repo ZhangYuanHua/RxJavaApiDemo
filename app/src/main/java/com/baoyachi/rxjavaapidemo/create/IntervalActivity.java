426b286567d277eb6a7c812686c3b2d94d4496c1
baoyachi <liaoymxsdl@sina.com>
2016-07-18 10:37:08 +0800

    add RxJava interval operator

--- app/src/main/java/com/baoyachi/rxjavaapidemo/create/IntervalActivity.java
+++ app/src/main/java/com/baoyachi/rxjavaapidemo/create/IntervalActivity.java
@@ -6,6 +6,12 @@ import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.baoyachi.rxjavaapidemo.R;
+import com.baoyachi.rxjavaapidemo.util.LogUtil;
+
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * 日期：16/7/17 18:43
@@ -31,6 +37,25 @@ public class IntervalActivity extends AppCompatActivity
 
     private void init()
     {
+        Observable.interval(0,2, TimeUnit.SECONDS).subscribe(new Subscriber<Long>()
+        {
+            @Override
+            public void onCompleted()
+            {
+                LogUtil.i("onCompleted");
+            }
 
+            @Override
+            public void onError(Throwable e)
+            {
+                LogUtil.i("onError"+e.getMessage());
+            }
+
+            @Override
+            public void onNext(Long aLong)
+            {
+                LogUtil.i("onNext");
+            }
+        });
     }
 }
